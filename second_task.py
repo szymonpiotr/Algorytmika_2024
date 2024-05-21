645afa720c6403bb66cbe8c780b1c706b1c025f5
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2024-05-21 14:15:00 +0200

    Add files via upload

---
+++ second_task.py
@@ -0,0 +1,10 @@
+# Zdefiniuj funkcję sumuj_liste_list, która akcptuje listę liste liczb i zwraca 
+# sumę wszystkich liczb we wszystkich podlistach. Jako pomocniczą funkcję, możesz 
+# zdefiniować funkcję sumuj_liste_liczb, któa sumuje wszystkie liczby w liście liczb.
+# Użyj rekurencji. 
+# Działanie:
+# sumuj_liste_liczb([1,1,1]) = 3
+# sumuj_liste_list([[1,1,1],[1,2,3]]) = 9 
+
+ 
+   
