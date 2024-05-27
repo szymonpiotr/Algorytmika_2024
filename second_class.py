4decd5a8514d5158108369f7ef9e6f391af26504
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2024-05-27 09:51:20 +0200

    Add files via upload

---
+++ second_class.py
@@ -0,0 +1,19 @@
+# Sortowanie
+
+# 1a. Zdefinuj funkcję, która sortuje listę liczb - użyj rekurencji. Jeśli jest to potrzebne
+# zdefiniuj funkcje pomocnicze. 
+# 1b. Zdefinuj funkcję, które sortuje listę liczb - użyj iteracji.
+
+# 2a. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj rekurencji.
+# 2b. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj iteracji. 
+
+# 3a. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj rekurencji.
+# 3b. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj iteracji.
+
+# 4. Trójkąt Pacala. Zdefinuj funkcję, która dostaje dwa argumenty: numer kolumny i numer wiersza, zwraca zaś 
+# liczbą Pascala która na danej współrzędnej występuje
+
+# 5. Zdefiniuj funkcję "balance", która sprawdza, czy w danym Stringu nawiasy są ustawione w 
+# prawidłowy sposób, tj. czy każde lewy nawias ma swój prawy nawias do pary i czy są one dobrze ustawione.
+
+
