f3257217b1db175f1309bbfc8c1c56fd2301b503
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2024-06-11 14:29:47 +0200

    Update trees.py

--- trees.py
+++ trees.py
@@ -3,18 +3,18 @@ class Node:
       self.left = None
       self.right = None
       self.data = data
+
 root = Node(1)
 root.left = Node(2)
-root.left.left = Node(10)
-root.left.left.left = Node(14)
-root.left.right = Node(11)
-root.left.right.left = Node(12)
 root.right = Node(3)
-root.right.left = Node(4)
-root.right.left.left = Node(13)
-root.right.right = Node(5)
-root.right.right.left = Node(6)
-root.right.right.right = Node(7) 
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.left.left.left = Node(8)
+root.left.left.right = Node(9)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.right.right.left = Node(10)
+root.right.right.right = Node(11)
 
 def is_element(root, element):
    if root.left == None and root.right == None:
