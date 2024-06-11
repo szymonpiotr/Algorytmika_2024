d9e820e1a84efc2b1957bc2198d295353c4ad9cd
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2024-06-11 14:11:18 +0200

    Update trees.py

--- trees.py
+++ trees.py
@@ -3,11 +3,15 @@ class Node:
       self.left = None
       self.right = None
       self.data = data
-
 root = Node(1)
 root.left = Node(2)
+root.left.left = Node(10)
+root.left.left.left = Node(14)
+root.left.right = Node(11)
+root.left.right.left = Node(12)
 root.right = Node(3)
 root.right.left = Node(4)
+root.right.left.left = Node(13)
 root.right.right = Node(5)
 root.right.right.left = Node(6)
 root.right.right.right = Node(7) 
