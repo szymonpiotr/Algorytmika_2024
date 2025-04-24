cb1ae35a4bc5768d556745488d29735c06bec487
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2025-04-24 20:15:29 +0200

    Update trees.py

--- trees.py
+++ trees.py
@@ -15,29 +15,3 @@ root.right.left = Node(6)
 root.right.right = Node(7)
 root.right.right.left = Node(10)
 root.right.right.right = Node(11)
-
-def is_element(root, element):
-   if root.left == None and root.right == None:
-      return element == root.data
-   else:
-      return (is_element(root.left, element) or is_element(root.right, element))
-
-def how_many(root):
-   if root.left == None and root.right == None:
-      return 1 
-   else:
-      return how_many(root.left) + how_many(root.right) + 1 
-   
-def is_balanced(root):
-   if root.left == None and root.right == None:
-      return True
-   else:
-      return abs(how_many(root.left) - how_many(root.right)) <= 1
-
-def subtree_balanced(root):
-   if root.left == None and root.right == None:
-      return True
-   else:
-      return is_balanced(root) and subtree_balanced(root.left) and subtree_balanced(root.right)
-
-  
