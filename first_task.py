7510dd418aa31d5372534f9c35a8783c77b92101
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2025-04-24 20:05:57 +0200

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -1,2 +1,1 @@
-# Zdefinuj funkcję "listToSet", która usuwa z listy elementy powarzające się. Zdefiniuj funkcję pomocniczą sprawdzającą
-# czy dany obiekt znajduje się na liście. Użyj rekurencji. 
+# TBA
