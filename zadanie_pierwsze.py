3ad446ece08297023fb27fedb5e6e1fdf75dbe9b
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2025-05-23 10:26:52 +0200

    Update zadanie_pierwsze.py

--- zadanie_pierwsze.py
+++ zadanie_pierwsze.py
@@ -1,1 +1,4 @@
-# TBA
+# Zdefiniuj funkcję "listToSet", która usuwa z 
+# listy elementy powtarzające się. 
+# Zdefiniuj funkcję pomocniczą sprawdzającą
+# czy dany obiekt znajduje się na liście. Użyj rekurencji.
