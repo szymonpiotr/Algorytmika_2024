8666a5ce8db473b903005ce8637e344db21e5418
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2025-06-06 08:54:14 +0200

    Update sortowanie.py

--- sortowanie.py
+++ sortowanie.py
@@ -2,8 +2,25 @@
 
 # 1a. Zdefinuj funkcję, która sortuje listę liczb - użyj rekurencji. Jeśli jest to potrzebne
 # zdefiniuj funkcje pomocnicze. 
+def recurSortt(list):
+    if list == []:
+        return []
+    else:
+        pivot = list[0]
+        smaller = [i for i in list[1:] if i < pivot]
+        larger = [i for i in list[1:] if i >= pivot]
+        return recurSortt(smaller) + [pivot]  + recurSortt(larger) 
 
 # 1b. Zdefinuj funkcję, które sortuje listę liczb - użyj iteracji.
+def iterSort(list):
+    for step in range(1, len(list)):
+        key = list[step]
+        j = step - 1     
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j = j - 1
+        list[j + 1] = key
+    return list  
 
 
 # 2a. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj rekurencji.
@@ -18,6 +35,12 @@
 
 # 4. Trójkąt Pacala. Zdefinuj funkcję, która dostaje dwa argumenty: numer kolumny i numer wiersza, zwraca zaś 
 # liczbą Pascala która na danej współrzędnej występuje.
+
+def pascal(wiersz, kolumna):
+    if (kolumna == 0 or wiersz == kolumna):
+        return 1   
+    else:
+        return pascal(wiersz - 1, kolumna - 1) + pascal(wiersz-1, kolumna)
 
 
 # 5. Zdefiniuj funkcję "balance", która sprawdza, czy w danym Stringu nawiasy są ustawione w 
